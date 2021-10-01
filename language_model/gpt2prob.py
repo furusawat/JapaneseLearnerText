2a0a7c01843dd677e19b0e863ec3c3024b7509f6
furusawat <63394367+furusawat@users.noreply.github.com>
2021-10-01 18:30:10 +0900

    Implement LM-Critic (with very simple configuration)

--- language_model/gpt2prob.py
+++ language_model/gpt2prob.py
@@ -1,11 +1,16 @@
 from transformers import T5Tokenizer, AutoModelForCausalLM
+import torch
 import numpy as np
 import json
+import perturb
+
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 tokenizer = T5Tokenizer.from_pretrained("rinna/japanese-gpt2-medium")
 tokenizer.do_lower_case = True
 
 model = AutoModelForCausalLM.from_pretrained("rinna/japanese-gpt2-medium")
+model = model.to(device)
 
 with open("result.json") as fp:
     rightlist = json.load(fp)
@@ -13,15 +18,23 @@ with open("result.json") as fp:
 with open("result.json.origin") as fp:
     wronglist = json.load(fp)
 
-for i in range(len(rightlist)):
-    tokens = tokenizer.encode(rightlist[i]["sent"], add_special_tokens=False, return_tensors="pt")
+def gpt2test(text, tries = 100):
+    tokens = tokenizer.encode(text, add_special_tokens=False, return_tensors="pt").to(device)
     loss = model(tokens, labels = tokens)[0]
     rightprob = np.exp(loss.cpu().detach().numpy())
-    print("{} : {}".format(rightlist[i]["sent"], rightprob))
 
-    tokens = tokenizer.encode(wronglist[i]["sent"], add_special_tokens=False, return_tensors="pt")
-    loss = model(tokens, labels = tokens)[0]
-    wrongprob = np.exp(loss.cpu().detach().numpy())
-    print("{} : {}".format(wronglist[i]["sent"], wrongprob))
-    print("{} must be lower than {} : {}".format(rightprob, wrongprob, "yes" if rightprob < wrongprob else "no"))
+    lowernum = 0
+    for i in range(tries):
+        tmptext = perturb.perturb(text)
+        tokens = tokenizer.encode(tmptext, add_special_tokens=False, return_tensors="pt").to(device)
+        loss = model(tokens, labels = tokens)[0]
+        if np.exp(loss.cpu().detach().numpy()) < rightprob:
+            lowernum += 1
+
+    return lowernum
+
+
+for i in range(len(rightlist)):
+    print("{} : {}".format(rightlist[i]["sent"], gpt2test(rightlist[i]["sent"])))
+    print("{} : {}".format(wronglist[i]["sent"], gpt2test(wronglist[i]["sent"])))
     print()
