777ef0cd4d88a7b79370557591e82ffb417070f3
furusawat <63394367+furusawat@users.noreply.github.com>
2021-10-05 15:50:27 +0900

    Modify roberta2prob.py (adding word vectors similarity)

--- language_model/roberta2prob.py
+++ language_model/roberta2prob.py
@@ -4,6 +4,9 @@ import numpy as np
 import json
 import perturb
 import copy
+from gensim.models import KeyedVectors
+import os
+import pickle
 
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
@@ -12,6 +15,34 @@ tokenizer.do_lower_case = True
 
 model = RobertaForMaskedLM.from_pretrained("rinna/japanese-roberta-base")
 model = model.to(device)
+print("== model loaded ==")
+
+vecpath = "./vecsim_data.pickle"
+if not os.path.isfile(vecpath):
+    vec = KeyedVectors(vector_size = next(model.parameters()).shape[1])
+    words = []
+    for eachtoken in range(next(model.parameters()).shape[0]):
+        words.append(eachtoken)
+    vec.add_vectors(words, next(model.parameters()).tolist())
+    print("== word vectors loaded ==")
+
+    worddic = {}
+    for i, eachword in enumerate(words):
+        if i % 5000 == 4999:
+            print("== {} word vectors similarity calculated ==".format(i))
+        worddic[eachword] = {k:v for k,v in vec.similar_by_word(eachword, topn = int(len(words)/1000))}
+        selfposition = 0
+        for k,v in worddic[eachword].items():
+            if v > eachword:
+                break
+            selfposition += 1
+        worddic[eachword][eachword] = selfposition
+
+    with open(vecpath, "wb") as fp:
+        pickle.dump(worddic, fp)
+else:
+    with open(vecpath, "rb") as fp:
+        worddic = pickle.load(fp)
 
 with open("result.json") as fp:
     rightlist = json.load(fp)
@@ -37,9 +68,11 @@ def robertaeval(orig_txt):
         with torch.no_grad():
             outs = model(input_ids = txt_tensor, position_ids = posi_id_tensor)
 
-        max_ids = outs[0][0, i].topk(1).indices
-        score = torch.nn.functional.softmax(outs[0][0,i], dim=0)
-        score = score[orig_idx] / score[max_ids[0]]
+        outs = outs[0].index_select(2, torch.LongTensor(list(worddic[orig_idx].keys())).to(device))
+
+        max_ids = outs[0, i].topk(1).indices
+        score = torch.nn.functional.softmax(outs[0,i], dim=0)
+        score = score[worddic[orig_idx][orig_idx]] / score[max_ids[0]]
 
         tmplist.append(float(score))
 
@@ -47,24 +80,24 @@ def robertaeval(orig_txt):
     for eachscore in tmplist:
         allscore += eachscore
 
-    return [tmplist, allscore/len(tmplist)]
+    return [tmplist, allscore/ (len(tmplist) ** 2)]
 
 
 def roberta2test(text, tries = 100):
     rightprob = robertaeval(text)
-    print("{} : {} -> avg. {}".format(text, str(rightprob[0]), rightprob[1]))
-    print("*"*40)
+    print("{} : {} \n -> avg. with length-penalty {}".format(text, str(rightprob[0]), rightprob[1]))
+    print("*"*80)
 
     lowernum = 0
     for i in range(tries):
         tmptext = perturb.perturb(text)
         wrongprob = robertaeval(tmptext)
         if wrongprob[1] > rightprob[1]:
-            print("{} : {} \n -> avg. {}".format(tmptext, str(wrongprob[0]), wrongprob[1]))
+            print("{} : {} \n -> avg. with length-penalty {}".format(tmptext, str(wrongprob[0]), wrongprob[1]))
             lowernum += 1
 
-    print("*"*40)
-    print("# of successful perturbation : {}\t: lower is better".format(lowernum))
+    print("*"*80)
+    print("# of successful perturbation : {}\t(lower is better)".format(lowernum))
 
 
 for i in range(len(rightlist)):
